3d3c0c4154af2e696eae4db8f1aacd3cd34ad4bf
Eduardo Dias <eduvidias@gmail.com>
2024-03-11 16:29:35 -0300

    teste

--- arquivos/par_impar.c
+++ arquivos/par_impar.c
@@ -1,10 +1,16 @@
 #include "queue.h"
+#include <time.h>
+
+srand(time(NULL));
 
 int main () {
   int i, n = 10;	
   /*Use somente as operações definidas em queue.h*/
   /*Não use para resolver esse exercício o operador -> aqui!*/
   /*Terminar*/
+  for (i = 0; i < n; i++) {
+    
+  }
   return 0;
 }
 
