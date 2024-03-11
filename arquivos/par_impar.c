45841e9de7833825ae658705cd4d35677ecf59db
Eduardo Dias <eduvidias@gmail.com>
2024-03-11 18:03:39 -0300

    par impar

--- arquivos/par_impar.c
+++ arquivos/par_impar.c
@@ -1,16 +1,34 @@
 #include "queue.h"
 #include <time.h>
 
-srand(time(NULL));
 
 int main () {
   int i, n = 10;	
   /*Use somente as operações definidas em queue.h*/
   /*Não use para resolver esse exercício o operador -> aqui!*/
   /*Terminar*/
-  for (i = 0; i < n; i++) {
+  i = 0;
+  Queue *impar = create(n);
+  Queue *par = create(n);
+
+  srand(time(NULL));
+
+  while (i < n) {
+    int elem = rand() % 11;
     
+    if (elem % 2) { // é impar
+      enqueue(impar, elem);
+    }
+    else {
+      enqueue(par, elem);
+    }
+    i++;
   }
+  print(impar);
+  print(par);
+  destroy (impar);
+  destroy (par);
+
   return 0;
 }
 
