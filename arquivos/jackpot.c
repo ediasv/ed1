6dcb0a911c45c366ae17000b5524ec345f14a0f1
Eduardo Dias <eduvidias@gmail.com>
2024-03-14 14:45:45 -0300

    jackpot funcional

--- arquivos/jackpot.c
+++ arquivos/jackpot.c
@@ -1,26 +1,43 @@
 #include "queue.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
 void jackpot(int n, int r) {
   Queue **carreteis;
-  int i, j;
-  int elem;
+  int i, j, elem, venceu;
 
+  // Criando (e preenchendo) n carreteis de (r + 1) posicoes
   carreteis = (Queue **)malloc(sizeof(Queue *) * n);
   for (i = 0; i < n; i++) {
     carreteis[i] = create(r + 1);
+
     elem = rand() % 10;
-    while (!full(carreteis[i])) {
+
+    for (j = 0; j < r; j++) {
       enqueue(carreteis[i], elem);
       if (elem < 9)
         elem++;
       else
         elem = 0;
     }
-    // print(carreteis[i]);
   }
+
+  for (i = 0; i < 6; i++) {
+    for (j = 0; j < n; j++) {
+      elem = dequeue(carreteis[j]);
+      enqueue(carreteis[j], elem);
+      printf("%d ", elem);
+    }
+    printf("\n");
+  }
+
+  venceu = front(carreteis[0]);
+  for (i = 1; i < n; i++) {
+    if (venceu != front(carreteis[i]))
+      venceu = -1;
+  }
+
+  if (venceu != -1)
+    printf("VOCE VENCEU!\n");
 
   for (i = 0; i < n; i++) {
     destroy(carreteis[i]);
