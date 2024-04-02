f9b0ef4660089dfdce2be9f1a77037ebbb32de71
Eduardo Dias <eduvidias@gmail.com>
2024-04-02 19:42:26 -0300

    stack.c

--- ll-aprofundamento/list.c
+++ ll-aprofundamento/list.c
@@ -1,25 +1,19 @@
 #include "list.h"
 
-List* create () {
-  return NULL;
-}
+List *create() { return NULL; }
 
-void destroy (List *l) {
-  /*Terminar*/	
-}
+void destroy(List *l) { /*Terminar*/ }
 
-int empty (List *l) {
-  /*Terminar*/	
-}
+int empty(List *l) { /*Terminar*/ }
 
-List* insert (List *l, int elem) {
+List *insert(List *l, int elem) {
   List *n = (List *)malloc(sizeof(List));
   n->data = elem;
   n->next = l;
-  return n;  
+  return n;
 }
 
-void print (List *l) {
+void print(List *l) {
   List *t;
   printf("List: ");
   for (t = l; t != NULL; t = t->next) {
--- ll-aprofundamento/stack.c
+++ ll-aprofundamento/stack.c
@@ -1,29 +1,48 @@
 #include "stack.h"
 
-Stack* create () {
-  /*Terminar*/
+Stack *create() {
+  Stack *s = NULL;
+  return s;
 }
 
-void destroy (Stack *s) {
-  /*Terminar*/
+void destroy(Stack *s) {
+  Stack *tmp = s;
+  while (s->next != NULL) {
+    s = s->next;
+    free(tmp);
+    tmp = s;
+  }
+  free(s);
 }
 
-Stack* push (Stack *s, int elem) {
-  /*Terminar*/
+Stack *push(Stack *s, int elem) {
+  Stack *tmp = (Stack *)malloc(sizeof(Stack));
+  tmp->data = elem;
+  tmp->next = s;
+  return tmp;
 }
 
-Stack* pop (Stack *s) {
-  /*Terminar*/
+Stack *pop(Stack *s) {
+  Stack *tmp = s;
+  s = s->next;
+  return tmp;
 }
 
-int get_peek (Stack *s) {
-  /*Terminar*/
+int get_peek(Stack *s) {
+  if (empty(s))
+    return ERROR;
+  return s->data;
 }
 
-int empty (Stack *s) {
-  /*Terminar*/
-}
+int empty(Stack *s) { return (s == NULL); }
 
-void print (Stack *s) {
-  /*Terminar*/
+void print(Stack *s) {
+  if (!empty(s)) {
+    Stack *tmp = s;
+    while (tmp != NULL) {
+      printf("%d ", tmp->data);
+      tmp = tmp->next;
+    }
+    printf("\n");
+  }
 }
