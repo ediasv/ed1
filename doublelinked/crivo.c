f2951954e5cdad1015574e29a48f7eb0461d8db3
Eduardo Dias <eduvidias@gmail.com>
2024-04-19 19:01:01 -0300

    estudos

--- doublelinked/crivo.c
+++ doublelinked/crivo.c
@@ -1,13 +1,26 @@
 #include "list.h"
 #include <math.h>
 
+List *remove_multiplos(List *l, int limite) {
+  int i = 2;
+  List *current = l, *previous = l;
+  while (i < limite) {
+    current while (current->next != NULL) {
+      if (current->data % i == 0)
+        remove_queue(l, current->data);
+    }
+    previous = previous->next;
+  }
+  return l;
+}
+
 void crivo(int n) {
   int i, sq = sqrt(n);
   List *l = create();
   /*Terminar!*/
-  for (i = 2; i <= n; i++) {
-    // oi
-  }
+  for (i = 2; i <= n; i++)
+    insert_back(l, i);
+  l = remove_multiplos(l, sq);
   destroy(l);
 }
 
